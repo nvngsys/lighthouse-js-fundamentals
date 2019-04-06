55d9de4ef2817738e04fd3d9ebcc095450b20b9f
Jack Brennan <jack.brennan@nvngsystems.com>
2019-04-06 15:28:15 +0000

    odd.js exercise with 'isOdd' function completed

---
+++ odd.js
@@ -0,0 +1,9 @@
+// This function should take a number as a parameter and return true if it is odd and false otherwise.
+function isOdd(num){
+  return num % 2 !== 0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("-3 is odd: " + isOdd(-3));
+console.log("-8 is odd: " + isOdd(-8));
